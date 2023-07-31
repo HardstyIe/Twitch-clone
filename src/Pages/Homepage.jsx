6ca909cace19a889fb220455cfab0ce69b6e9b60
HardstyIe <hardstyle.dd@gmail.com>
2023-07-31 22:24:05 +0200

    fix of the oauth

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,17 +1,37 @@
-import Cookies from "js-cookie";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import RightsideHomepage from "../Components/RighsideHomepage";
 import Sidebar from "../Components/Sidebar";
 
 
 const Homepage = () => {
-  console.log(import.meta.env.VITE_CLIENT_ID)
+  const [data, setData] = useState(null);
+
   const redirectToTwitchAuth = () => {
     const clientId = import.meta.env.VITE_CLIENT_ID;
-    const redirectUri = encodeURIComponent(import.meta.env.VITE_REDIRECT_URI);
+    const redirectUri = import.meta.env.VITE_REDIRECT_URI;
     const scope = encodeURIComponent(import.meta.env.VITE_SCOPE);
     window.location = `https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&state=c3ab8aa609ea11e793ae92361f002671`;
   };
+
+  useEffect(() => {
+    const authToken = localStorage.getItem("authToken");
+
+    fetch('https://api.twitch.tv/helix/', {
+      method: 'GET',
+      headers: {
+        'Client-ID': import.meta.env.VITE_CLIENT_ID,
+        'Authorization': `Bearer ${authToken}`
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        setData(data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }, []);
+
 
   useEffect(() => {
     // Parse the hash fragment in the URL
@@ -28,20 +48,23 @@ const Homepage = () => {
 
     // Set token
     if (hash.access_token) {
-      Cookies.set("authToken", hash.access_token, { secure: true });
-      console.log(Cookies.get("authToken"));
+      localStorage.setItem("authToken", hash.access_token);
+      console.log('Token:', localStorage.getItem("authToken")); // Debug log to inspect token
+    } else {
+      console.log('Access token not found in hash'); // Debug log if access token not found
     }
   }, []);
+
 
   return (
     <div className={"w-full flex"}>
       <div className={"w-1/5"}>
         <button onClick={redirectToTwitchAuth}>Se connecter avec Twitch</button>
-
         <Sidebar />
       </div>
       <div className={"w-4/5"}>
         <RightsideHomepage />
+        {data ? JSON.stringify(data) : 'Loading...'}
       </div>
     </div>
   );
